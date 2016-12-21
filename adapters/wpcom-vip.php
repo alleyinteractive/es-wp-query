33b1c1f57f8e8345e911b6178d0b4cf5b702f546
Jack Lenox <jacklenox@fastmail.com>
2016-12-21 21:27:57 +0000

    Update adapter to handle additional case
    Props @kasparsd

--- adapters/wpcom-vip.php
+++ adapters/wpcom-vip.php
@@ -74,6 +74,7 @@ class ES_WP_Query extends ES_WP_Query_Wrapper {
 function vip_es_field_map( $es_map ) {
 	return wp_parse_args( array(
 		'post_author'                   => 'author_id',
+		'post_author.user_login'        => 'author_login',
 		'post_author.user_nicename'     => 'author_login',
 		'post_date'                     => 'date',
 		'post_date.year'                => 'date_token.year',

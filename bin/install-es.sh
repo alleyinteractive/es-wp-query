4103cd3f7946aa0ad87e829beff59207818d7f72
Matt Boynes <mboynes@alleyinteractive.com>
2017-09-14 10:56:59 -0400

    Set network.host for 5.x

--- bin/install-es.sh
+++ bin/install-es.sh
@@ -29,7 +29,7 @@ fi
 
 # java_home='/usr/lib/jvm/java-8-oracle'
 if [[ "$ES_VERSION" == 5.* ]]; then
-  start_es '-Epath.repo=/tmp'
+  start_es '-Epath.repo=/tmp -Enetwork.host=0.0.0.0'
 else
   start_es '-Des.path.repo=/tmp'
 fi

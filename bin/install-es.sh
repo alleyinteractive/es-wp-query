ddbb6bc6b3765a742130400601209cc4bb25b942
Matt Boynes <mboynes@alleyinteractive.com>
2017-09-14 23:57:38 -0400

    Allow travis to test ES 6

--- bin/install-es.sh
+++ bin/install-es.sh
@@ -20,17 +20,15 @@ start_es() {
   /tmp/elasticsearch/bin/elasticsearch $1 > /tmp/elasticsearch.log &
 }
 
-if [[ "$ES_VERSION" == 5.* ]]; then
-  setup_es https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
-elif [[ "$ES_VERSION" == 2.* ]]; then
+if [[ "$ES_VERSION" == 2.* ]]; then
   setup_es https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ES_VERSION}/elasticsearch-${ES_VERSION}.tar.gz
-elif [[ "$ES_VERSION" == 1.* ]]; then
-  setup_es https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
+else
+  setup_es https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
 fi
 
 # java_home='/usr/lib/jvm/java-8-oracle'
-if [[ "$ES_VERSION" == 5.* ]]; then
-  start_es '-Epath.repo=/tmp -Enetwork.host=_local_'
-else
+if [[ "$ES_VERSION" == 2.* ]]; then
   start_es '-Des.path.repo=/tmp'
+else
+  start_es '-Epath.repo=/tmp -Enetwork.host=_local_'
 fi

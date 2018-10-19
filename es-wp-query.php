4c5a125d46a9c2676749a26a90176e0868290437
Kevin Fodness <kevin@alley.co>
2018-10-19 16:44:47 -0400

    Apply phpcs fixes to es-wp-query.php

--- es-wp-query.php
+++ es-wp-query.php
@@ -1,28 +1,30 @@
 <?php
+/**
+ * Plugin Name: Elasticsearch Wrapper for WP_Query
+ * Plugin URI: https://github.com/alleyinteractive/es-wp-query
+ * Description: A drop-in replacement for WP_Query to leverage Elasticsearch for complex queries.
+ * Version: 0.2
+ * Author: Matthew Boynes
+ * Author URI: http://www.alleyinteractive.com/
+ *
+ * @package ES_WP_Query
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
-/*
-	Plugin Name: Elasticsearch Wrapper for WP_Query
-	Plugin URI: https://github.com/alleyinteractive/es-wp-query
-	Description: A drop-in replacement for WP_Query to leverage Elasticsearch for complex queries.
-	Version: 0.2
-	Author: Matthew Boynes
-	Author URI: http://www.alleyinteractive.com/
-*/
-/*
-  This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+// phpcs:disable WordPressVIPMinimum.Files.IncludingFile.IncludingFile
 
 define( 'ES_WP_QUERY_PATH', dirname( __FILE__ ) );
 

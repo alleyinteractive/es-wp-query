884e89b45f1ed9d9fdddf6c67ff12c37b273dc07
lipemat <mat@matlipe.com>
2020-01-09 10:50:45 -0500

    Remove superfluous line break

--- class-es-wp-query-shoehorn.php
+++ class-es-wp-query-shoehorn.php
@@ -81,7 +81,6 @@ function es_wp_query_shoehorn( &$query ) {
 		$es_query_args['is_main_query'] = $query->is_main_query();
 		$es_query                = new ES_WP_Query( $es_query_args );
 
-
 		// Make the post query use the post IDs from the ES results instead.
 		$query->parse_query(
 			array(

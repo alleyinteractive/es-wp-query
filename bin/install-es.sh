70f068146d947adf5e473ba6f89e0305c6add4ce
Matthew Boynes <git@mattboynes.com>
2020-04-15 13:19:51 -0400

    Update tests to work with ES 7.x

--- bin/install-es.sh
+++ bin/install-es.sh
@@ -6,7 +6,6 @@ if [ $# -lt 1 ]; then
 fi
 
 ES_VERSION=$1
-echo $JAVA_HOME
 
 setup_es() {
   download_url=$1
@@ -20,14 +19,17 @@ start_es() {
   /tmp/elasticsearch/bin/elasticsearch $1 > /tmp/elasticsearch.log &
 }
 
-if [[ "$ES_VERSION" == 2.* ]]; then
+if [[ "$ES_VERSION" == 1.* ]]; then
+  setup_es https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
+elif [[ "$ES_VERSION" == 2.* ]]; then
   setup_es https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ES_VERSION}/elasticsearch-${ES_VERSION}.tar.gz
-else
+elif [[ "$ES_VERSION" == [56].* ]]; then
   setup_es https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
+else
+  setup_es https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}-linux-x86_64.tar.gz
 fi
 
-# java_home='/usr/lib/jvm/java-8-oracle'
-if [[ "$ES_VERSION" == 2.* ]]; then
+if [[ "$ES_VERSION" == [12].* ]]; then
   start_es '-Des.path.repo=/tmp'
 else
   start_es '-Epath.repo=/tmp -Enetwork.host=_local_'

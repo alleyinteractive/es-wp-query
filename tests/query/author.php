14fb9b114eaa427a5bfc66c99c059ba5e88fc7ad
Linnea Huxford <7308162+mslinnea@users.noreply.github.com>
2024-12-12 22:24:38 -0800

    update test setup function

--- tests/query/author.php
+++ tests/query/author.php
@@ -7,7 +7,7 @@
  * @group author
  */
 class Tests_Query_Author extends WP_UnitTestCase {
-	function setup(): void {
+	public function setUp(): void {
 		parent::setUp();
 		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%/' );
 	}
